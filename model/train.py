a8051a288a6b9b2c6cddbc3e670f7b97bc52e946
Phong <phong940253@gmail.com>
2021-11-07 00:25:47 +0700

    update train

--- model/train.py
+++ model/train.py
@@ -3,13 +3,15 @@ from torch_geometric.data import InMemoryDataset, Data, DataLoader
 import torch
 import pandas as pd
 from sklearn.model_selection import GroupShuffleSplit
+from sklearn.metrics import roc_auc_score, precision_recall_fscore_support
 import numpy as np
 from tqdm import tqdm
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, TopKPooling, global_mean_pool as gap, global_max_pool as gmp
+from torch.nn import Sequential as Seq, Linear, ReLU
+from torch_geometric.nn import GCNConv, TopKPooling, global_mean_pool as gap, global_max_pool as gmp, MessagePassing
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
-import torch
+from torch_geometric.utils import remove_self_loops, add_self_loops
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -31,15 +33,20 @@ users = pd.read_csv("../pygcn/data/BX-Users.csv",
 
 book_id_encoder = LabelEncoder()
 book_id_encoder.fit(pd.concat([books['ISBN'], voting['ISBN']]))
-books['ISBN'] = book_id_encoder.transform(books['ISBN'])
-voting['ISBN'] = book_id_encoder.transform(voting['ISBN'])
+# books['ISBN'] = book_id_encoder.transform(books['ISBN'])
+# voting['ISBN'] = book_id_encoder.transform(voting['ISBN'])
 
 book_title_encoder = LabelEncoder()
 books['Book-Title'] = book_title_encoder.fit_transform(books['Book-Title'])
 
+
+book_author_encoder = LabelEncoder()
+books['Book-Author'].fillna("", inplace=True)
+books['Book-Author'] = book_author_encoder.fit_transform(books['Book-Author'])
+
 book_publisher_encoder = LabelEncoder()
-books['Publisher'] = book_publisher_encoder.fit_transform(
-    books['Publisher'])
+books['Publisher'].fillna("", inplace=True)
+books['Publisher'] = book_publisher_encoder.fit_transform(books['Publisher'])
 
 user_location_encoder = LabelEncoder()
 users['Location'] = user_location_encoder.fit_transform(users['Location'])
@@ -73,27 +80,27 @@ class BookDataset(InMemoryDataset):
     def process(self):
         # Read data into huge `Data` list.
         data_list = []
-
-        grouped = voting.groupby('User-ID')
+        new_voting = voting.join(books.set_index('ISBN'), on='ISBN')
+        n_voting = new_voting.join(users.set_index('User-ID'), on='User-ID')
+    
+        grouped = n_voting.groupby('User-ID')
         for user_id, group in tqdm(grouped):
-            user_book_id = LabelEncoder().fit_transform(group['ISBN'])
+            user_book_id = book_id_encoder.transform(group['ISBN'])
             group = group.reset_index(drop=True)
             group['user_book_id'] = user_book_id
-            # print("\nuser book id: ", type(user_book_id))
-            # print(group)
-
+            print("\nuser book id: ", user_book_id)
+            print(group)
             node_features = group.loc[group['User-ID'] == user_id,
-                                      ['user_book_id', 'ISBN']].sort_values('user_book_id')["ISBN"].values
-            # print(node_features)
-
-            node_features = torch.LongTensor(node_features).unsqueeze(1)
+                                      ['user_book_id', 'ISBN', "Book-Title", 'Book-Author', 'Year-Of-Publication', 'Publisher', 'Age', 'Location']].sort_values('user_book_id')[['ISBN', "Book-Title", 'Book-Author', 'Year-Of-Publication', 'Publisher', 'Age', 'Location']].values
+            print(node_features)
+            node_features = torch.from_numpy(node_features)
             target_nodes = group['user_book_id'].values[1:]
             source_nodes = group['user_book_id'].values[:-1]
 
             edge_index = torch.tensor(
                 [source_nodes, target_nodes], dtype=torch.long)
             x = node_features
-            y = torch.IntTensor([group['Book-Rating'].values[0]])
+            y = torch.FloatTensor([group['Book-Rating'].values[0]])
             data = Data(x=x, edge_index=edge_index, y=y)
             data_list.append(data)
 
@@ -116,15 +123,52 @@ test_loader = DataLoader(test_dataset, batch_size=512, shuffle=True)
 
 embed_dim = 128
 
+class CUSConv(MessagePassing):
+    def __init__(self, in_channels, out_channels):
+        super(CUSConv, self).__init__(aggr='max') #  "Max" aggregation.
+        self.lin = torch.nn.Linear(in_channels, out_channels)
+        self.act = torch.nn.ReLU()
+        self.update_lin = torch.nn.Linear(in_channels + out_channels, in_channels, bias=False)
+        self.update_act = torch.nn.ReLU()
+        
+    def forward(self, x, edge_index):
+        # x has shape [N, in_channels]
+        # edge_index has shape [2, E]
+        
+        
+        edge_index, _ = remove_self_loops(edge_index)
+        edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
+        
+        
+        return self.propagate(edge_index, size=(x.size(0), x.size(0)), x=x)
+
+    def message(self, x_j):
+        # x_j has shape [E, in_channels]
+
+        x_j = self.lin(x_j)
+        x_j = self.act(x_j)
+        
+        return x_j
+
+    def update(self, aggr_out, x):
+        # aggr_out has shape [N, out_channels]
+
+
+        new_embedding = torch.cat([aggr_out, x], dim=1)
+        
+        new_embedding = self.update_lin(new_embedding)
+        new_embedding = self.update_act(new_embedding)
+        
+        return new_embedding
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = GCNConv(embed_dim, 128)
+        self.conv1 = CUSConv(embed_dim, 128)
         self.pool1 = TopKPooling(128, ratio=0.8)
-        self.conv2 = GCNConv(128, 128)
+        self.conv2 = CUSConv(128, 128)
         self.pool2 = TopKPooling(128, ratio=0.8)
-        self.conv3 = GCNConv(128, 128)
+        self.conv3 = CUSConv(128, 128)
         self.pool3 = TopKPooling(128, ratio=0.8)
         self.item_embedding = torch.nn.Embedding(
             num_embeddings=voting['ISBN'].max() + 1, embedding_dim=embed_dim)
@@ -183,6 +227,11 @@ def evaluate(loader):
             label = data.y.detach().cpu().numpy()
             predictions.append(pred)
             labels.append(label)
+    
+    predictions = np.hstack(predictions)
+    labels = np.hstack(labels)
+    
+    return precision_recall_fscore_support(labels, predictions, average='micro')
 
 
 def train():
@@ -206,10 +255,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
 crit = torch.nn.BCELoss()
 for epoch in range(100):
     loss = train()
-    train_acc = evaluate(train_loader)
-    val_acc = evaluate(valid_loader)
-    test_acc = evaluate(test_loader)
-    print('Epoch: {:03d}, Loss: {:.5f}, Train Auc: {:.5f}, Val Auc: {:.5f}, Test Auc: {:.5f}'.
-          format(epoch, loss, train_acc, val_acc, test_acc))
-
-torch.save(model.state_dict(), "weight.pt")
+    train_p, train_r, train_f, _ = evaluate(train_loader)
+    valid_p, valid_r, valid_f, _ = evaluate(valid_loader)
+    test_p, test_r, test_f, _ = evaluate(test_loader)
+    print('Epoch: {:03d}, Loss: {:.5f}, Train P: {:.5f} R: {:.5f} F: {:.05f}, Val P: {:.5f} R: {:.5f} F: {:.5f}, Test Auc P: {:.5f} R: {:.5f} F: {:.5f}.'.format(epoch, loss, train_p, train_r, train_f, valid_p, valid_r, valid_f, test_p, test_r, test_f))
